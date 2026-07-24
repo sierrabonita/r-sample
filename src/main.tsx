a0c69aab3cecfb5bd1ae78dc138ec4e48895a977
sierrabonita <workbj1212@gmail.com>
2026-07-24 13:00:56 +0900

    feat: @tanstack/react-router をインストール

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from '@/components/ui/provider';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
 import './index.css';
-import App from '@/App';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const queryClient = new QueryClient();
 
@@ -11,7 +20,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
