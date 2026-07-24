dc7a2467845ce678e38de9b758ce9f88c4ca497c
sierrabonita <workbj1212@gmail.com>
2026-07-24 12:26:06 +0900

    feat: ChakraUI をインストール

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from '@/components/ui/provider';
 import './index.css';
 import App from '@/App';
 
@@ -9,7 +10,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </StrictMode>,
 );
