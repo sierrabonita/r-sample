4dad3df2845314f42d55a76ef26c01c46e851e3e
sierrabonita <workbj1212@gmail.com>
2026-08-06 10:12:36 +0900

    refactor: Home画面でのデータ取得仮実装

--- src/routes/_authenticated/home/index.tsx
+++ src/routes/_authenticated/home/index.tsx
@@ -1,35 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Box, Center, HStack, Heading, List, Spinner, Text } from '@chakra-ui/react';
+import { useGetReviews } from '@/usecases/useReviews';
 
 export const Route = createFileRoute('/_authenticated/home/')({
   component: Home,
 });
 
-// ダミーデータ
-const fetching = false;
-const data = {
-  reviews: [
-    {
-      id: 1,
-      content: 'レビュー・コメント',
-      isSpoiler: 0,
-      createdAt: '2026-07-25',
-      user: { name: 'ユーザー名' },
-      movie: { title: '映画タイトル' },
-    },
-  ],
-};
-
 function Home() {
-  if (fetching) {
+  const { data: reviews = [], isLoading } = useGetReviews();
+
+  if (isLoading) {
     return (
       <Center h="100vh">
         <Spinner size="xl" />
       </Center>
     );
   }
-
-  const reviews = data?.reviews ?? [];
 
   if (!reviews.length) {
     return <Box>レビューがありません</Box>;
